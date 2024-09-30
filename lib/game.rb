25fd2018519ff0a0c62205e3d09179713207822c
tlx02 <e0774379@u.nus.edu>
2024-09-30 13:32:17 +0800

    Add print statements before getting user input

--- lib/game.rb
+++ lib/game.rb
@@ -9,14 +9,19 @@ class Game
     playerId = 1
 
     while true
+      puts "Player #{playerId}'s turn!"
+      puts "Row:"
       row = Integer(gets.chomp)
+      puts "Column:"
       col = Integer(gets.chomp)
+
       @state.insertSymbol(row, col, playerId)
       @state.printState
       if @state.playerWon?(playerId)
         puts "Player #{playerId} won!"
         return
       end
+      puts " "
       playerId = playerId == 2 ? 1 : 2
     end
   end
